481ff724555b57a28f2effdc9fbeab9aeb1443ee
Valen Brata <valen.brata3@gmail.com>
2021-02-17 14:42:02 +0700

    Penambahan notification berhasil update status

--- app/src/main/java/com/androidatc/lesson07_e3_androidintent/SecondActivity.kt
+++ app/src/main/java/com/androidatc/lesson07_e3_androidintent/SecondActivity.kt
@@ -1,13 +1,21 @@
 package com.androidatc.lesson07_e3_androidintent
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -20,6 +28,7 @@ class SecondActivity : AppCompatActivity() {
         HelloMessage_ID.text = message
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun update(view: View) {
         val UpdateAlert= AlertDialog.Builder(this)
 
@@ -29,6 +38,32 @@ class SecondActivity : AppCompatActivity() {
         UpdateAlert.setPositiveButton("Yes") {
                 dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Updated",
             Toast.LENGTH_LONG).show()
+            val id = "updated"
+
+            val name = getString(R.string.abc_action_bar_home_description)
+
+            val description = getString(R.string.abc_action_bar_home_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(id, name, importance)
+
+            mChannel.description = description
+            mChannel.enableLights(true)
+
+            mChannel.lightColor = Color.RED
+            mChannel.enableVibration(true)
+
+            val CHANNEL_ID = "updated"
+
+            // Use NotificationCompat.Builder to add the notification objects
+            val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon_background)
+                .setContentTitle("Status Updated")
+                .setContentText("Status has been updated!")
+
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            mNotificationManager.createNotificationChannel(mChannel)
+            mNotificationManager.notify(1, mBuilder.build())
         }
         UpdateAlert.setNegativeButton("No") {
                 dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Canceled",
