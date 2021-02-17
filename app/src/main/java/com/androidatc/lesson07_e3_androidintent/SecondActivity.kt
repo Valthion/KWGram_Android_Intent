edd3dba0c784a958400b2be2a2638f68ca4ed817
Valen Brata <valen.brata3@gmail.com>
2021-02-17 13:45:36 +0700

    Pembuatan alert untuk update

--- app/src/main/java/com/androidatc/lesson07_e3_androidintent/SecondActivity.kt
+++ app/src/main/java/com/androidatc/lesson07_e3_androidintent/SecondActivity.kt
@@ -1,9 +1,12 @@
 package com.androidatc.lesson07_e3_androidintent
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
@@ -16,6 +19,25 @@ class SecondActivity : AppCompatActivity() {
         var message = intent.getStringExtra("name")
         HelloMessage_ID.text = message
     }
+
+    fun update(view: View) {
+        val UpdateAlert= AlertDialog.Builder(this)
+
+        UpdateAlert.setTitle("Update Status")
+        UpdateAlert.setMessage("Are you sure you want to save changes?")
+
+        UpdateAlert.setPositiveButton("Yes") {
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Updated",
+            Toast.LENGTH_LONG).show()
+        }
+        UpdateAlert.setNegativeButton("No") {
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Canceled",
+            Toast.LENGTH_LONG).show()
+        }
+
+        UpdateAlert.show()
+    }
+
     fun go2dmActivity(view: View) {
         var intent = Intent(this,DMActivity::class.java)
         startActivity(intent)
