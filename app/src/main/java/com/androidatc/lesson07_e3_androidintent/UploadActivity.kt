f9ca762a768f355a193a2faea77377bea29710b0
MatthewADIAN <68260159+MatthewADIAN@users.noreply.github.com>
2021-02-19 18:09:07 +0700

    upload widget

--- app/src/main/java/com/androidatc/lesson07_e3_androidintent/UploadActivity.kt
+++ app/src/main/java/com/androidatc/lesson07_e3_androidintent/UploadActivity.kt
@@ -1,9 +1,14 @@
 package com.androidatc.lesson07_e3_androidintent
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.activity_third.*
 
@@ -11,6 +16,36 @@ class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth)
+    }
+
+
+    fun GallerySnack(view: View) {
+        var SnackB: Snackbar = Snackbar.make(findViewById(R.id.constraintLayout)
+            ,"You Choose From Gallery",Snackbar.LENGTH_LONG).setAction("Retry") {}
+
+        SnackB.setActionTextColor(Color.RED)
+        SnackB.show()
+    }
+
+
+    fun upload(view: View) {
+        val SaveAlert= AlertDialog.Builder(this)
+
+        SaveAlert.setTitle("Upload")
+        SaveAlert.setMessage("Are you sure you want to upload")
+
+        SaveAlert.setPositiveButton("Yes") {
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Uploaded",
+            Toast.LENGTH_LONG).show()
+        }
+        SaveAlert.setNegativeButton("No") {
+
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Upload cancelled",
+            Toast.LENGTH_LONG).show()
+
+        }
+
+        SaveAlert.show()
     }
 
     fun go2secondActivity(view: View){
