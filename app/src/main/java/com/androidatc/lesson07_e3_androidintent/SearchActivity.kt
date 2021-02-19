7e5c7ea80ae2f6deb204557d37cdc85c29dbe160
yogdrax <68434751+yogdrax@users.noreply.github.com>
2021-02-19 21:41:28 +0700

    Update Search Activity+Widget

--- app/src/main/java/com/androidatc/lesson07_e3_androidintent/SearchActivity.kt
+++ app/src/main/java/com/androidatc/lesson07_e3_androidintent/SearchActivity.kt
@@ -1,9 +1,14 @@
 package com.androidatc.lesson07_e3_androidintent
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.FirstName_ID
 import kotlinx.android.synthetic.main.activity_second.*
@@ -12,5 +17,31 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
+    }
+
+    fun about(view :View){
+        val SaveAlert= AlertDialog.Builder(this)
+        SaveAlert.setTitle("Save Picture")
+        SaveAlert.setMessage("Are you sure you want save image to Gallery?")
+        SaveAlert.setPositiveButton("Yes") {
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Saved",
+            Toast.LENGTH_LONG).show()
+
+        }
+        SaveAlert.setNegativeButton("No") {
+
+                dialogInterface: DialogInterface?, which: Int -> Toast.makeText(applicationContext,"Cancelled",
+            Toast.LENGTH_LONG).show()
+
+        }
+
+        SaveAlert.show()
+    }
+    fun SearchSnack(view: View) {
+        var SnackB: Snackbar = Snackbar.make(findViewById(R.id.constraintLayout)
+            ,"No internet Connection", Snackbar.LENGTH_LONG).setAction("Retry") {}
+
+        SnackB.setActionTextColor(Color.RED)
+        SnackB.show()
     }
 }
